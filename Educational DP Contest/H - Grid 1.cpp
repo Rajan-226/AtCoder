a78c4c45fca3ff4fa5c1fe33a56a13137ebf2158
Rajan-226 <56467449+Rajan-226@users.noreply.github.com>
2020-10-17 17:54:48 +0530

    Create H - Grid 1.cpp

---
+++ Educational DP Contest/H - Grid 1.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define mxsize (int)1e6+5
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+void fun(){
+    int n,m;
+    cin>>n>>m;
+    vector<ll> dp(m+1,0);
+    dp[1]=1;
+    fi(1,n+1){
+        fj(1,m+1){
+            char c;
+            cin>>c;
+            if(c!='#')      dp[j]=(dp[j-1]+dp[j])%mod;
+            else            dp[j]=0;
+        }
+    }
+    cout<<dp[m]<<"\n";
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
