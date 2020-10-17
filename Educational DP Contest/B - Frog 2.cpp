d81f8f330fa2bed32398de0193969b8db62165c4
Rajan-226 <56467449+Rajan-226@users.noreply.github.com>
2020-10-17 17:50:13 +0530

    Create B - Frog 2.cpp

---
+++ Educational DP Contest/B - Frog 2.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define UM unordered_map<int,int>
+#define MP map<int,int>
+#define mxsize (int)1e6
+void fun(){
+    int n,k;
+    cin>>n>>k;
+    int h[n];
+    fi(0,n) cin>>h[i];
+    int dp[n];
+    dp[0]=0;
+    fi(1,n){
+        dp[i]=INT_MAX;
+        fj(max(0,i-k),i){
+            dp[i]=min(dp[i],dp[j]+abs(h[j]-h[i]));
+        }
+    }
+    cout<<dp[n-1]<<"\n";
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
